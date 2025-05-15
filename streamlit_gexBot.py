cfec7be864193342c1d237b6b17e090afa5fbd25
icebergdelphi <iceberg350@hotmail.com>
2025-05-15 16:18:14 -0500

    Create streamlit_gexBot.py

---
+++ streamlit_gexBot.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from streamlit.components.v1 import html
+
+# Título de la aplicación
+st.title("GexBot Embed en Streamlit")
+
+# HTML personalizado con el iframe
+html_code = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>GexBot Embed</title>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+    }
+    .iframe-container {
+      width: 100%;
+      max-width: 2000px;
+      margin: 0 auto;
+    }
+    iframe {
+      width: 100%;
+      height: 90vh;
+      border: 2px solid black;
+    }
+  </style>
+</head>
+<body>
+  <div class="container-fluid">
+    <br>
+    <br>
+    <div class="iframe-container">
+      <iframe 
+        src="https://www.gexbot.com/"
+        title="Contenido externo"
+        allowfullscreen
+      ></iframe>
+    </div>
+  </div>
+</body>
+</html>
+"""
+
+# Renderizar el HTML en Streamlit
+html(html_code, height=900)
