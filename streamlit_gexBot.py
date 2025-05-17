c9e861477e6cd5116dac83cf1ae64e496c492b19
icebergdelphi <iceberg350@hotmail.com>
2025-05-17 06:33:56 -0500

    Update streamlit_gexBot.py

--- streamlit_gexBot.py
+++ streamlit_gexBot.py
@@ -86,12 +86,12 @@ html_code = """
     /* Ajustes responsivos para diferentes tamaños de pantalla /
     @media (max-height: 768px) {
       iframe {
-        height: 100vh; / Para pantallas pequeñas 100 /
+        height: 760px; / Para pantallas pequeñas 100 /
       }
     }
     @media (min-height: 769px) and (max-height: 900px) {
       iframe {
-        height: 90vh; / Para pantallas medianas 90/
+        height: 760px; / Para pantallas medianas 90/
       }
     }
     @media (min-height: 901px) {
