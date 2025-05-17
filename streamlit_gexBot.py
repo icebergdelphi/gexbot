9ebeb9e77a1443993070ed64ee52d4f4e6fdc668
icebergdelphi <iceberg350@hotmail.com>
2025-05-17 11:43:51 -0500

    Update streamlit_gexBot.py

--- streamlit_gexBot.py
+++ streamlit_gexBot.py
@@ -73,14 +73,6 @@ html_code = """
       background-color: #000;
     }
     
-    .iframe-wrapper {
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100vh;
-      overflow: hidden;
-    }
     
     iframe {
       width: 100%;
