3ec0fd9dff61d8401d6de8c9a3d5d821c6e48214
icebergdelphi <iceberg350@hotmail.com>
2025-05-17 05:59:51 -0500

    Update streamlit_gexBot.py

--- streamlit_gexBot.py
+++ streamlit_gexBot.py
@@ -75,7 +75,7 @@ html_code = """
       height:0%; / Altura dinámicamente ajustada al viewport 100/
     }
     iframe {
-      width: 100vw; / Ocupa todo el ancho del viewport /
+      width: 50vw; / Ocupa todo el ancho del viewport 100 /
       height: 70vh; / Altura dinámicamente ajustada al viewport 100/
       border: none; / Elimina el borde /
       display: block; / Asegura que no haya espacios extra */
